9be947596242d6cba7b1a4467dffea577f63af6f
David Prevost <david.prevost@goto.com>
2024-10-20 08:15:54 -0400

    chore: Example app with 30 audio file + README
    fix: mp3 file copy not working
    - Existing copy logic was not working anymore
    - Other changes comes from lint
    
    chore: problem processing around 30 audio files
    
    chore: addd iOS installation instruction for example app
    
    chore: add gemfile lock after iOS installation
    
    chore: make multiple audio files work on iOS
    
    Revert max candle changes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -76,7 +76,7 @@ const RenderListItem = React.memo(
       } else {
         ref.current?.startPlayer({ finishMode: FinishMode.stop });
       }
-    }, [currentPlaying]);
+    }, [currentPlaying, item.path]);
 
     return (
       <View key={item.path} style={[styles.listItemContainer]}>
@@ -84,8 +84,8 @@ const RenderListItem = React.memo(
           <View style={[styles.buttonContainer]}>
             <Pressable
               disabled={isLoading}
-              onPress={handleButtonAction}
-              style={styles.playBackControlPressable}>
+              style={styles.playBackControlPressable}
+              onPress={handleButtonAction}>
               {isLoading ? (
                 <ActivityIndicator color={'#FF0000'} />
               ) : (
@@ -139,8 +139,8 @@ const RenderListItem = React.memo(
             />
             {playerState === PlayerState.playing ? (
               <Pressable
-                onPress={changeSpeed}
-                style={[styles.speedBox, styles.whiteBackground]}>
+                style={[styles.speedBox, styles.whiteBackground]}
+                onPress={changeSpeed}>
                 <Text style={styles.speed}>{`${currentPlaybackSpeed}x`}</Text>
               </Pressable>
             ) : (
@@ -175,7 +175,7 @@ const LivePlayerComponent = ({
 
   const handleRecorderAction = async () => {
     if (recorderState === RecorderState.stopped) {
-      let hasPermission = await checkHasAudioRecorderPermission();
+      const hasPermission = await checkHasAudioRecorderPermission();
 
       if (hasPermission === PermissionStatus.granted) {
         startRecording();
@@ -206,8 +206,8 @@ const LivePlayerComponent = ({
         onRecorderStateChange={setRecorderState}
       />
       <Pressable
-        onPress={handleRecorderAction}
-        style={styles.recordAudioPressable}>
+        style={styles.recordAudioPressable}
+        onPress={handleRecorderAction}>
         <Image
           source={
             recorderState === RecorderState.stopped ? Icons.mic : Icons.stop
@@ -251,10 +251,10 @@ const AppContainer = () => {
   return (
     <View style={styles.appContainer}>
       <StatusBar
-        barStyle={'dark-content'}
-        backgroundColor={'transparent'}
         animated
         translucent
+        barStyle={'dark-content'}
+        backgroundColor={'transparent'}
       />
       <GestureHandlerRootView style={styles.appContainer}>
         <View style={styles.screenBackground}>
